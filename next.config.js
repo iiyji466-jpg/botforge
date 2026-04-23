71b1ad211942290d2f8c12f183220d794d70f42a
iiyji466-jpg <iiyji466@gmail.com>
2026-04-23 17:17:41 +0300

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    GEMINI_API_KEY: "ضع_مفتاح_جمناي_الخاص_بك_هنا",
+  },
 }
 
 module.exports = nextConfig
